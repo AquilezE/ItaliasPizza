17bb2898bbd56899fd261480ff5e713c26702bb9
AquilezE <pochitaisking@gmail.com>
2025-04-21 14:32:54 -0600

    -Se agregó el UserControl PedidoPreview con propiedades de dependencia: IdPedido, Total,Mesa, Fecha -Se añadió un botón "Ver Detalles" en PedidoPreview que lanza el evento VerDetallesClicked -Se actualizó MenuVerPedidos.xaml para usar ScrollViewer y ItemsControl para mostrar pedidos con PedidoPreview
    -Se agregó una ObservableCollection<Pedido> en MenuVerPedidos.xaml.cs
    -Se creó el método CargarPedidos para enlazar datos y cargar pedidos de prueba
    
    -Se crearon nuevas páginas:
    +DetallePedidoLlevar.xaml
    +DetallePedidoLocal.xaml
    
    -Se mejoró el diseño de PedidoPreview
    -Se agregó lógica temporal en el evento VerDetallesClicked para manejar navegación a detalles

--- ItaliasPizzaCliente/Paginas/MenuPedidoPages/MenuVerPedidos.xaml.cs
+++ ItaliasPizzaCliente/Paginas/MenuPedidoPages/MenuVerPedidos.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ItaliasPizzaCliente.Singletons;
+using ItaliasPizzaDB.DataAccessObjects;
+using ItaliasPizzaDB.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,53 @@ namespace ItaliasPizzaCliente.Paginas.MenuPedidoPages
     /// </summary>
     public partial class MenuVerPedidos : Page
     {
+
+        public ObservableCollection<Pedido> Pedidos { get; set; } = new ObservableCollection<Pedido>();
+
         public MenuVerPedidos()
         {
             InitializeComponent();
+            this.DataContext = this;
+            CargarPedidos(UsuarioSingleton.Instance.NombreCargo);
+
         }
+
+        private void PedidoPreview_VerDetallesClicked(object sender, EventArgs e)
+        {
+            var preview = (PedidoPreview)sender;
+
+            var pedido = (Pedido)preview.DataContext;
+            
+            switch (pedido)
+            {
+                case PedidoParaLocal _:
+                    
+                    //TODO: Aqui no se qpd alch
+
+                default:
+
+                    break;
+            }
+            
+        }
+
+        private void CargarPedidos(string instanceNombreCargo)
+        {
+            var list = new List<Pedido>();
+
+            list.Add(new Pedido()
+            {
+                IdPedido = 1,
+                Total = 100,
+                Fecha = DateTime.Now,
+            });
+
+
+            foreach (var p in list)
+                Pedidos.Add(p);
+
+        }
+
+
     }
 }
