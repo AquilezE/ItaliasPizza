5cc99f19ad4d0b778b5f4038a0d3872dedd4df7e
TeDeCanela <153479319+TeDeCanela@users.noreply.github.com>
2025-05-06 17:19:08 -0600

    codifciación de interfaces: CU05, CU06, CU07, CU08, CU10, CU11, CU12

--- ItaliasPizzaCliente/Paginas/MenuClientePages/MenuGestionarDireccionCliente.xaml.cs
+++ ItaliasPizzaCliente/Paginas/MenuClientePages/MenuGestionarDireccionCliente.xaml.cs
@@ -13,21 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ItaliasPizzaCliente.Paginas.MenuInventarioPages
+namespace ItaliasPizzaCliente.Paginas.MenuClientePages
 {
     /// <summary>
-    /// Interaction logic for MenuModificarInsumo.xaml
+    /// Lógica de interacción para MenuGestionarDireccionCliente.xaml
     /// </summary>
-    public partial class MenuModificarInsumo : Page
+    public partial class MenuGestionarDireccionCliente : Page
     {
-        public MenuModificarInsumo()
+        public MenuGestionarDireccionCliente()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
